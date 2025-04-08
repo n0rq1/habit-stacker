21ccea1f6d84902de54ce4698d2b3fd5b31c52ca
Austin <anorquist@mail.csuchico.edu>
2025-04-08 11:43:37 -0700

    signup/in/out works now. just need to deploy

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
-const { signupSchema } = require("../middlewares/validator");
+const jwt = require('jsonwebtoken');
+const { signupSchema, signinSchema } = require("../middlewares/validator");
 const User = require('../models/usersModel');
-const { doHash } = require('../utils/hashing');
+const { doHash, doHashValidation } = require('../utils/hashing');
 
 exports.signup = async (req, res) => {
     const {email, password} = req.body;
@@ -31,4 +32,43 @@ exports.signup = async (req, res) => {
     } catch(error){
         console.log(error); 
     }
+}
+
+exports.signin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const {error, value} = signinSchema.validate({email, password});
+        if (error){
+            return res.status(401).json({success:false, message: error.details[0].message});
+        }
+
+        const existingUser = await User.findOne({email}).select('+password');
+        if(!existingUser){
+            return res.status(401).json({success: false, message: 'user does not exist'});
+        }
+        const result = await doHashValidation(password, existingUser.password);
+        if (!result){
+            return res.status(401).json({success: false, message: 'invalid credentials'});
+        }
+        const token = jwt.sign({
+            userId: existingUser._id,
+            email: existingUser.email,
+            verified: existingUser.verified,
+        }, process.env.TOKEN_SECRET);
+
+        res.cookie('Authorization', 'Bearer' + token, {expires: new Date(Date.now() + 8
+            *3600000), httpOnly: process.env.NODE_ENV === 'production', 
+            secure: process.env.NODE_ENV === 'production'}).json({
+                success: true,
+                token,
+                message: "logged in successfully",
+        });
+
+    } catch (error){
+        console.log(error);
+    }
+}
+
+exports.signout = async (req, res) => {
+    res.clearCookie('Authorization').status(200).json({success: true, message: "logged out successfully"});
 }
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,5 +3,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
+router.post('/signout', authController.signout);
 
 module.exports = router;
