8d4e3458ac4cc8ac5a82ec0f41c6a3ff55722dfb
Austin <anorquist@mail.csuchico.edu>
2025-04-22 22:46:35 -0700

    testing api

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const { authenticateUser } = require('../middlewares/auth');
 const User = require('../models/usersModel'); 
 
 exports.user = (req, res) => {
+    console.log("here");
     if (!req.user) return res.status(401).json({ message: 'Unauthorized' });
     console.log(req.user._id);
     res.json({
